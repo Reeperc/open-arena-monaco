960d0999fee90e352d0b055a8aaf82b8c1485ae0
Reeperc <taher974.karimdjee@gmail.com>
2024-05-15 12:13:59 +0200

    dfg

--- new/CréationCompteJoueur.php
+++ new/CréationCompteJoueur.php
@@ -3,9 +3,9 @@ session_start(); // Démarrer la session
 
 // Afficher le message de la variable de session
 if (isset($_SESSION['message'])) {
-    echo "<p>{$_SESSION['message']}</p>";
-    // Supprimer le message de la session pour ne pas l'afficher à nouveau
-    unset($_SESSION['message']);
+  echo "<p>{$_SESSION['message']}</p>";
+  // Supprimer le message de la session pour ne pas l'afficher à nouveau
+  unset($_SESSION['message']);
 }
 /*
 // Afficher le message d'erreur de creation de compte s'il est présent dans l'URL
@@ -14,18 +14,20 @@ if (isset($_GET['errorMessage'])) {
 }
 */
 
+require('database.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Récupérer les données du formulaire
-    $nom = $_POST["nom"];
-    $prenom = $_POST["prenom"];
-    $pseudo = $_POST["pseudo"];
-    $mot_de_passe = password_hash($_POST["mot_de_passe"], PASSWORD_DEFAULT);
+  // Récupérer les données du formulaire
+  $nom = $_POST["nom"];
+  $prenom = $_POST["prenom"];
+  $pseudo = $_POST["pseudo"];
+  $mot_de_passe = password_hash($_POST["mot_de_passe"], PASSWORD_DEFAULT);
 
-    try {
-        // Établir la connexion à la base de données
 
-        /*
+  try {
+    // Établir la connexion à la base de données
+
+    /*
         $serveur = "localhost";
         $utilisateur = "grp_6_10";
         $motDePasse = "oPkO06vqDtnh";
@@ -36,49 +38,49 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         */
 
-        require('database.php');
-
-        // Préparer la requête SQL d'insertion
-        $query = "INSERT INTO Joueur (Nom, Prénom, Email, username, password) VALUES (?, ?, ?, ?, ?)";
-        $stmt = $connexion->prepare($query);
-
-        $mail="@arena-monaco.fr";
-        $email=$pseudo . $mail ;
-
-        // Exécuter la requête avec les valeurs
-        $stmt->execute([$nom, $prenom, $email, $pseudo, $mot_de_passe]);
 
 
-        // Message de succès dans une variable de session
-        $_SESSION['message'] = "Ajout du joueur $email réussi.";
+    // Préparer la requête SQL d'insertion
+    $query = "INSERT INTO Joueur (Nom, Prénom, Email, username, password) VALUES (?, ?, ?, ?, ?)";
+    $stmt = $connexion->prepare($query);
 
-        // Rediriger vers la page "AccueilAdminF.php" après l'inscription réussie
-        header("Location: AccueilAdminF.php");
+    $mail = "@arena-monaco.fr";
+    $email = $pseudo . $mail;
 
-        exit(); // Assurez-vous de terminer l'exécution du script après la redirection
+    // Exécuter la requête avec les valeurs
+    $stmt->execute([$nom, $prenom, $email, $pseudo, $mot_de_passe]);
 
-    } catch (PDOException $e) {
-      // En cas d'erreur, vérifier si c'est une erreur d'intégrité (doublon de clé unique)
-      if ($e->errorInfo[1] == 1062) {
-          $_SESSION['error_message'] = "Ce pseudo existe déjà.";
-            // Rediriger vers la page d'inscription avec un message d'erreur
-          header("Location: ErreurAjoutJoueur.php");
-          //header("Location: CréationCompteJoueur.php" );
-          exit();
-          
-        } else {
-          // Autre erreur, afficher l'erreur
-          echo "Erreur lors de la creation de l'ID : " . $e->getMessage();
-      }
+
+    // Message de succès dans une variable de session
+    $_SESSION['message'] = "Ajout du joueur $email réussi.";
+
+    // Rediriger vers la page "AccueilAdminF.php" après l'inscription réussie
+    header("Location: AccueilAdminF.php");
+
+    exit(); // Assurez-vous de terminer l'exécution du script après la redirection
+
+  } catch (PDOException $e) {
+    // En cas d'erreur, vérifier si c'est une erreur d'intégrité (doublon de clé unique)
+    if ($e->errorInfo[1] == 1062) {
+      $_SESSION['error_message'] = "Ce pseudo existe déjà.";
+      // Rediriger vers la page d'inscription avec un message d'erreur
+      header("Location: ErreurAjoutJoueur.php");
+      //header("Location: CréationCompteJoueur.php" );
+      exit();
+    } else {
+      // Autre erreur, afficher l'erreur
+      echo "Erreur lors de la creation de l'ID : " . $e->getMessage();
+    }
   } finally {
-      // Fermer la connexion
-      $connexion = null;
+    // Fermer la connexion
+    $connexion = null;
   }
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -109,7 +111,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       margin-top: 10px;
     }
 
-    input, button {
+    input,
+    button {
       margin-top: 5px;
     }
 
@@ -119,34 +122,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       padding: 10px;
       cursor: pointer;
     }
+
     #return-button {
-     position: fixed;
-     bottom: 20px;
-     right: 20px;
-     background-color: #007bff;
-     color: #fff;
-     padding: 10px;
-     border: none;
-     cursor: pointer;
+      position: fixed;
+      bottom: 20px;
+      right: 20px;
+      background-color: #007bff;
+      color: #fff;
+      padding: 10px;
+      border: none;
+      cursor: pointer;
     }
   </style>
 </head>
+
 <body>
-<?php include('MenuAdminF.php'); ?>
+  <?php include('MenuAdminF.php'); ?>
   <main>
     <h1>Inscrivez le joueur</h1>
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Récupérer les données du formulaire
-        $nom = $_POST["nom"];
-        $prenom = $_POST["prenom"];
-        $pseudo = $_POST["pseudo"];
-        $mot_de_passe = $_POST["mot_de_passe"];
+      // Récupérer les données du formulaire
+      $nom = $_POST["nom"];
+      $prenom = $_POST["prenom"];
+      $pseudo = $_POST["pseudo"];
+      $mot_de_passe = $_POST["mot_de_passe"];
     }
     ?>
     <form action="" method="post">
       <div id="contact">
-        <p id="content"> Prenom <input type="text" name="prenom" placeholder="Jean" required> </p>        
+        <p id="content"> Prenom <input type="text" name="prenom" placeholder="Jean" required> </p>
         <p id="content"> Nom <input type="text" name="nom" placeholder="DUPONT" required></p>
         <p id="content"> Pseudo <input type="text" name="pseudo" placeholder="toto" required></p>
         <p id="content"> Nouveau Mot de passe : <input type="password" name="mot_de_passe" required> </p>
@@ -159,4 +164,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <p>Votre adresse mail est de la forme pseudo@arena-monaco.fr </p>
   <?php include('FooterF.php'); ?>
 </body>
+
 </html>
--- new/database.php
+++ new/database.php
@@ -7,16 +7,16 @@
 // $baseDeDonnees = "bdd_6_10";
 
 //BDD Localhost Serveur Web
-$serveur = "localhost";
-$utilisateur = "mo";
-$motDePasse = "mdp";
-$baseDeDonnees = "bdd_6_10";
+// $serveur = "localhost";
+// $utilisateur = "root";
+// $motDePasse = "root";
+// $baseDeDonnees = "bdd_6_10";
 
 //BDD moduleweb
-// $serveur = "moduleweb.esigelec.fr";
-// $utilisateur = "grp_6_10";
-// $motDePasse = "oPkO06vqDtnh";
-// $baseDeDonnees = "bdd_6_10";
+$serveur = "moduleweb.esigelec.fr";
+$utilisateur = "grp_6_10";
+$motDePasse = "oPkO06vqDtnh";
+$baseDeDonnees = "bdd_6_10";
 
 
 try {
