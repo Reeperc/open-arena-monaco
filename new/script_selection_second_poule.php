fd1d179f4c76ffd37c2740eafc1ceace644dd75e
Reeperc <taher974.karimdjee@gmail.com>
2024-05-15 15:43:30 +0200

    dth

--- new/script_selection_second_poule.php
+++ new/script_selection_second_poule.php
@@ -1,27 +1,24 @@
 <?php
 
-function script_selecion_second_poule($poule) {
+function script_selecion_second_poule($poule)
+{
 
     try {
         require('database.php');
-        $query = "SELECT equipe_gagnante, COUNT(*) AS victoires FROM matchs WHERE equipe_gagnante IS NOT NULL AND poule = '$poule' GROUP BY equipe_gagnante ORDER BY victoires DESC LIMIT 1, 1";
+        $query = "SELECT equipe_gagnante, COUNT(*) AS victoires FROM matchs WHERE equipe_gagnante IS NOT NULL AND poule = :poule GROUP BY equipe_gagnante ORDER BY victoires DESC LIMIT 1, 1";
         $stmt = $connexion->prepare($query);
 
-        $stmt->execute([$poule]);
-        
-        $equipe1=$stmt->fetchColumn();
+        $stmt->bindParam(':poule', $poule);
+
+        $stmt->execute();
+
+        $equipe1 = $stmt->fetchColumn();
 
         echo $equipe1;
         return $equipe1;
-
     } catch (PDOException $e) {
         echo "Erreur lors de l'insertion dans la DB'" . $e->getMessage();
     } finally {
-        $connexion=null;
+        $connexion = null;
     }
-
-
 }
-
-
-?>
--- new/script_selection_vainqueurs_poule.php
+++ new/script_selection_vainqueurs_poule.php
@@ -1,28 +1,25 @@
 <?php
 
-function script_selecion_vainqueur_poule($poule) {
+function script_selecion_vainqueur_poule($poule)
+{
 
     try {
         require('database.php');
-        $query = "SELECT equipe_gagnante, COUNT(*) AS victoires FROM matchs WHERE equipe_gagnante IS NOT NULL AND poule = '$poule' GROUP BY equipe_gagnante ORDER BY victoires DESC LIMIT 2";
+        $query = "SELECT equipe_gagnante, COUNT(*) AS victoires FROM matchs WHERE equipe_gagnante IS NOT NULL AND poule = :poule GROUP BY equipe_gagnante ORDER BY victoires DESC LIMIT 2";
         $stmt = $connexion->prepare($query);
 
-        $stmt->execute([$poule]);
-        
-        $equipe1=$stmt->fetchColumn();
-        $equipe2=$stmt->fetchColumn(1);
+        $stmt->bindParam(':poule', $poule);
+
+        $stmt->execute();
+
+        $equipe1 = $stmt->fetchColumn();
+        $equipe2 = $stmt->fetchColumn(1);
 
         echo $equipe1;
         return $equipe1;
-
     } catch (PDOException $e) {
         echo "Erreur lors de l'insertion dans la DB'" . $e->getMessage();
     } finally {
-        $connexion=null;
+        $connexion = null;
     }
-
-
 }
-
-
-?>
