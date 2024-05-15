1f50f101ba252423ebf85eabf2dee0e393b426b7
Reeperc <taher974.karimdjee@gmail.com>
2024-05-15 15:52:53 +0200

    dfg

--- new/script_vainqueur_quart.php
+++ new/script_vainqueur_quart.php
@@ -1,27 +1,24 @@
 <?php
 
-function script_vainqueur_quart($match_id) {
+function script_vainqueur_quart($match_id)
+{
 
     try {
         require('database.php');
-        $query = "SELECT equipe_gagnante FROM matchs WHERE id='$match_id'";
+        $query = "SELECT equipe_gagnante FROM matchs WHERE id= :match_id";
         $stmt = $connexion->prepare($query);
 
-        $stmt->execute([$match_id]);
-        
-        $equipe=$stmt->fetchColumn();
+        $stmt->bindParam(':match_id', $match_id);
+
+        $stmt->execute();
+
+        $equipe = $stmt->fetchColumn();
 
         echo $equipe;
         return $equipe;
-
     } catch (PDOException $e) {
         echo "Erreur lors de l'insertion dans la DB'" . $e->getMessage();
     } finally {
-        $connexion=null;
+        $connexion = null;
     }
-
-
 }
-
-
-?>
