46683f5fb3831618463a07e9a926fcdb94b26f6b
Reeperc <taher974.karimdjee@gmail.com>
2024-05-16 17:27:56 +0200

    hjjkxk

--- new/jeu.sh
+++ new/jeu.sh
@@ -14,5 +14,4 @@ INNER_EOF
 ssh rt@195.221.30.1 << EOF
 export DISPLAY=:0
 openarena +set cl_renderer opengl1 +set r_mode -1 +set r_customwidth 1280 +set r_customheight 720 +r_fullscreen 1 +connect 195.221.30.65:27960
-
 EOF
