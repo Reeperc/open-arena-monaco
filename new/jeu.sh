e7fa06f5012adce472a2101da9f715fe9d95b534
Reeperc <taher974.karimdjee@gmail.com>
2024-05-16 18:36:16 +0200

    cvvvz

--- new/jeu.sh
+++ new/jeu.sh
@@ -1,17 +1,13 @@
-if ! systemctl is active --quiet openarena-server.service
-then
-    echo "Le service n'est pas encore lance"
-    sudo systemctl restart openarena-server
-else
-    echo "Le service est déjà lancé"
-fi
+SSH_OPTIONS="-o StrictHostKeyChecking=no"
 
-ssh joueur@195.221.30.2 << INNER_EOF &
+
+ssh -t $SSH_OPTIONS joueur@195.221.30.2 << INNER_EOF &
 export DISPLAY=:0
 openarena +set cl_renderer opengl1 +set r_mode -1 +set r_customwidth 1280 +set r_customheight 720 +r_fullscreen 1 +connect 195.221.30.65:27960
 INNER_EOF
 
-ssh rt@195.221.30.1 << EOF
+ssh -t $SSH_OPTIONS rt@195.221.30.1 << EOF
 export DISPLAY=:0
 openarena +set cl_renderer opengl1 +set r_mode -1 +set r_customwidth 1280 +set r_customheight 720 +r_fullscreen 1 +connect 195.221.30.65:27960
 EOF
+
