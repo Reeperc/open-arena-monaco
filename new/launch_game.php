da7ca7c108313749d7dcbe319e0b379b016fbed0
Reeperc <taher974.karimdjee@gmail.com>
2024-05-16 16:28:01 +0200

    ghf

--- new/launch_game.php
+++ new/launch_game.php
@@ -22,6 +22,6 @@
 // Fermeture de la connexion SSH
 // ssh2_disconnect($connection);
 
-shell_exec('./jeu.sh');
+shell_exec('./var/www/html/open_arena_equipe_4/new/jeu.sh');
 // Affichage du résultat
 echo '<div style="color: green; font-weight: bold;">Le jeu a été lancé.</div>';
