bcfd9936d71db578523ecce9b7a3bc524319c64b
Reeperc <taher974.karimdjee@gmail.com>
2024-05-16 15:44:30 +0200

    dgh

---
+++ new/jeu.sh
@@ -0,0 +1,18 @@
+if ! systemctl is active --quiet openarena-server.service
+then
+    echo "Le service n'est pas encore lance"
+    sudo systemctl restart openarena-server
+else
+    echo "Le service est déjà lancé"
+fi
+
+ssh joueur@195.221.30.2 << INNER_EOF &
+export DISPLAY =: 0
+openarena +set cl_renderer openg11 +set r_mode -1 +set r_customwidth 1280 +set r_customheight 720 +r_fullscreen 1 +connect 195.221.30.65:27960
+INNER_EOF
+
+ssh rt@195.221.30.1 << EOF
+export DISPLAY =: 0
+openarena +set cl_renderer openg11 +set t_mode -1 +set r_customwidth 1280 +set r_customheight 720 +r_fullscreen 1 +connect 195.221.30.65:27960
+
+EOF
--- new/launch_game.php
+++ new/launch_game.php
@@ -1,26 +1,27 @@
 <?php
-include 'configSsh.php';
+// include 'configSsh.php';
 
 // Connexion SSH
-$connection = ssh2_connect($server, 22);
-if (!$connection) {
-    die('Impossible d\'établir la connexion SSH.');
-}
+// $connection = ssh2_connect($server, 22);
+// if (!$connection) {
+//     die('Impossible d\'établir la connexion SSH.');
+// }
 
 // Authentification SSH
-if (!ssh2_auth_password($connection, $username, $password)) {
-    die('Échec de l\'authentification SSH.');
-}
+// if (!ssh2_auth_password($connection, $username, $password)) {
+//     die('Échec de l\'authentification SSH.');
+// }
 
 // Exécution de la commande à distance pour lancer le jeu
-$command = 'sudo systemctl start openarena-game';
-$stream = ssh2_exec($connection, $command);
-stream_set_blocking($stream, true);
-$output = stream_get_contents($stream);
-fclose($stream);
+// $command = './jeu.sh';
+// $stream = ssh2_exec($connection, $command);
+// stream_set_blocking($stream, true);
+// $output = stream_get_contents($stream);
+// fclose($stream);
 
 // Fermeture de la connexion SSH
-ssh2_disconnect($connection);
+// ssh2_disconnect($connection);
 
+shell_exec('./jeu.sh');
 // Affichage du résultat
 echo '<div style="color: green; font-weight: bold;">Le jeu a été lancé.</div>';
