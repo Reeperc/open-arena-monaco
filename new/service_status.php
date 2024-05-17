eb55a5e3112084ca4a30956b815d02b4561fc0d1
Reeperc <taher974.karimdjee@gmail.com>
2024-05-17 04:13:04 +0200

    ghj

--- new/service_status.php
+++ new/service_status.php
@@ -22,11 +22,11 @@ fclose($stream);
 // Fermeture de la connexion SSH
 ssh2_disconnect($connection);
 
-// Affichage du contenu brut du terminal
+// Affichagge du contenu brut du terminal pour voir si ça corresp bien
 echo '<pre>' . nl2br(htmlentities($output)) . '</pre>';
 
 // Traitement du résultat pour extraire le statut
-if (strpos($output, 'active (running)') !== false) {
+if (strpos($output, 'active (running)') == true) {
     echo '<div style="color: green; font-weight: bold;">Le service OpenArena est actif.</div>';
 } else {
     echo '<div style="color: red; font-weight: bold;">Le service OpenArena n\'est pas actif.</div>';
