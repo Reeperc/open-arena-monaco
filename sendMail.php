6903709f971c1322b0be2dc923ad512aea35cd4d
Reeperc <taher974.karimdjee@gmail.com>
2024-05-20 20:23:22 +0200

    mmm

--- sendMail.php
+++ sendMail.php
@@ -5,16 +5,17 @@ error_reporting(E_ALL);
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'vendor/phpmailer/phpmailer/src/Exception.php';
-require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
-require 'vendor/phpmailer/phpmailer/src/SMTP.php';
+require 'vendor/autoload.php';
 
 $mail = new PHPMailer(true);
 try {
     $mail->isSMTP();
-    $mail->Host = '195.221.30.17'; // Adresse IP du serveur de messagerie
-    $mail->SMTPAuth = false; // Activez SMTPAuth si nécessaire
-    $mail->Port = 25; // ou 587 si vous utilisez TLS
+    $mail->Host = '195.221.30.17';
+    $mail->SMTPAuth = false; // Changez à true si l'authentification est nécessaire
+    $mail->Username = 'username'; // Votre nom d'utilisateur SMTP
+    $mail->Password = 'password'; // Votre mot de passe SMTP
+    $mail->SMTPSecure = 'tls'; // Ou 'ssl' si vous utilisez SSL
+    $mail->Port = 587; // Port pour TLS
     $mail->setFrom('noreply@arena-monaco.fr', 'Monaco Arena');
     $mail->addAddress('roger@arena-monaco.fr', 'Roger');
     $mail->Subject = 'Test Mail';
