86031c6e2514145283cfd42278cd0ac6bee1b4be
Reeperc <taher974.karimdjee@gmail.com>
2024-05-20 20:03:07 +0200

    mm

--- sendMail.php
+++ sendMail.php
@@ -5,9 +5,9 @@ error_reporting(E_ALL);
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '/var/www/html/vendor/phpmailer/src/Exception.php';
-require '/var/www/html/vendor/phpmailer/src/PHPMailer.php';
-require '/var/www/html/vendor/phpmailer/src/SMTP.php';
+require 'vendor/phpmailer/phpmailer/src/Exception.php';
+require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require 'vendor/phpmailer/phpmailer/src/SMTP.php';
 
 $mail = new PHPMailer(true);
 try {
