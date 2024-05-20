8a28662ea7d5d5823dfd50f5c87abc415dc50319
Reeperc <taher974.karimdjee@gmail.com>
2024-05-20 21:07:35 +0200

    c

--- sendMail.php
+++ sendMail.php
@@ -1,21 +1,25 @@
 <?php
+//afficher toutes les erreurs dans le navigateur
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+//chargement des classes PHPMailer
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 require 'vendor/autoload.php'; // à verifier
 
+//creation d'une instance de PHPMailer et config SMTP
 $mail = new PHPMailer(true);
 try {
     // Configuration de PHPMailer pour utiliser SMTP
-    $mail->isSMTP();
+    $mail->isSMTP(); //indique à PHPMailer d'utiliser smtp pour envoyer les mails
     $mail->Host = '195.221.30.17'; // adresse IP de du serveur SMTP
     $mail->SMTPAuth = false; // ou true s'il faut s'authentifier
     // $mail->Username = 'username'; //  nom d'utilisateur SMTP
     // $mail->Password = 'password'; // mot de passe SMTP
     $mail->Port = 25; // port 25, port 587 pr tls
+    $mail->CharSet = 'UTF-8';
     $mail->SMTPSecure = ''; // Désactivé, utiliser 'tls' ou 'ssl' si besoin
     $mail->SMTPOptions = array( //ser t à désactiver la vérification des certificats SSL
         'ssl' => array(
@@ -23,25 +27,26 @@ try {
             'verify_peer_name' => false,
             'allow_self_signed' => true
         )
+
     );
 
     // Active le débogage SMTP
     // 0 = off (for production use)
-    // 1 = client messages
-    // 2 = client and server messages
+    // 1 = messages client 
+    // 2 = messages client et serveurs 
     // 3= le niveau de debugging le plus détaillé .
     $mail->SMTPDebug = 3;
 
     // Expéditeur
-    $mail->setFrom('noreply@arena-monaco.fr', 'Monaco Arena');
+    $mail->setFrom('organisateur@arena-monaco.fr', 'Monaco Arena');
 
     // Destinataire
     $mail->addAddress('roger@arena-monaco.fr', 'Roger'); // Modifier par l'adresse de votre destinataire
 
     // Contenu de l'email
     $mail->isHTML(true); // Définir le format de l'email à HTML
-    $mail->Subject = 'Test Mail depuis le site';
-    $mail->Body    = 'Ce message a été envoyé depuis le site web <b>en gras!!!!</b>';
+    $mail->Subject = 'Test Mail depuis le site2';
+    $mail->Body    = 'Ce message a été envoyé depuis le site web <b>en gras!!!!</b> et avec un 😊 EMOJI!';
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
