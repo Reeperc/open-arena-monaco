e4af8ac628a75680a1101cae14a4e1150b89c3bb
Reeperc <taher974.karimdjee@gmail.com>
2024-05-20 20:38:05 +0200

    a

--- sendMail.php
+++ sendMail.php
@@ -5,23 +5,44 @@ error_reporting(E_ALL);
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'vendor/autoload.php';
+require 'vendor/autoload.php'; // Assurez-vous que ce chemin est correct
 
 $mail = new PHPMailer(true);
 try {
+    // Configuration de PHPMailer pour utiliser SMTP
     $mail->isSMTP();
-    $mail->Host = '195.221.30.17';
-    $mail->SMTPAuth = true; // Changez à true si l'authentification est nécessaire
-    $mail->Username = 'rt'; // Votre nom d'utilisateur SMTP
-    $mail->Password = 'rt'; // Votre mot de passe SMTP
-    $mail->SMTPSecure = 'tls'; // Ou 'ssl' si vous utilisez SSL
-    $mail->Port = 25; // Port pour TLS
+    $mail->Host = '195.221.30.17'; // L'adresse IP de votre serveur SMTP
+    $mail->SMTPAuth = false; // Défini à true si une authentification est nécessaire
+    $mail->Port = 25; // Utilisation du port 25, ajustez selon votre configuration serveur
+    $mail->SMTPSecure = ''; // Désactivé, utilisez 'tls' ou 'ssl' si nécessaire
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+    );
+
+    // Active le débogage SMTP
+    // 0 = off (for production use)
+    // 1 = client messages
+    // 2 = client and server messages
+    $mail->SMTPDebug = 2;
+
+    // Expéditeur
     $mail->setFrom('noreply@arena-monaco.fr', 'Monaco Arena');
-    $mail->addAddress('roger@arena-monaco.fr', 'Roger');
+
+    // Destinataire
+    $mail->addAddress('roger@arena-monaco.fr', 'Roger'); // Modifier par l'adresse de votre destinataire
+
+    // Contenu de l'email
+    $mail->isHTML(true); // Définir le format de l'email à HTML
     $mail->Subject = 'Test Mail';
-    $mail->Body    = 'This is the email body';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
     $mail->send();
     echo 'Message has been sent';
 } catch (Exception $e) {
-    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+    echo 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo;
 }
