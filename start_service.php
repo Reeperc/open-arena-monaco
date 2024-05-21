44e31f1b92d17f34cbdc35821bab751c47275d8a
AntoinePh69 <antoine.philippe@groupe-esigelec.org>
2024-05-21 12:09:58 +0200

    efgeg

--- start_service.php
+++ start_service.php
@@ -2,15 +2,13 @@
 include 'configSsh.php';
 
 // Construction de la commande sshpass
-$command = "sshpass -p 'quake' ssh -o StrictHostKeyChecking=no quake@195.221.30.65 'openarena-server +set dedicated 2 +set net_port 27960 +set g_gametype 1 +map delta +set sv_hostname "Tournois Monaco"'";
+$command = "sshpass -p 'quake' ssh -o StrictHostKeyChecking=no quake@195.221.30.65 'openarena-server +set dedicated 2 +set net_port 27960 +set g_gametype 1 +map delta +set sv_hostname \"Tournois Monaco\"'";
 
-// Exécution de la commande
-$output = null;
-$return_var = null;
-exec($command, $output, $return_var);
+// Exécution de la commande avec shell_exec
+$output = shell_exec($command);
 
 // Vérification du résultat de la commande
-if ($return_var === 0) {
+if ($output !== null) {
     echo '<div style="color: green; font-weight: bold;">Le service a été démarré.</div>';
 } else {
     echo '<div style="color: red; font-weight: bold;">Échec du démarrage du service.</div>';
