9ce794c1f7394dd8483bd7196cad45a413de599c
AntoinePh69 <antoine.philippe@groupe-esigelec.org>
2024-05-22 14:46:44 +0200

    netcat

--- stop_service.php
+++ stop_service.php
@@ -6,13 +6,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $command = "sshpass -p 'quake' ssh -o StrictHostKeyChecking=no quake@195.221.30.65 'screen -S openarena-server -X quit'";
 
     // Exécution de la commande avec shell_exec
-    $output = shell_exec($command);
+    shell_exec($command);
 
-    // Vérification du résultat de la commande
-    if ($output !== null) {
-        echo '<div style="color: green; font-weight: bold;">La partie est fermé.</div>';
+    // Pause pour donner au serveur le temps de s'arrêter
+    sleep(2);
+
+    // Vérification de l'état du port 27961
+    $portCommand = "nc -zv 195.221.30.65 27961 2>&1";
+    $portOutput = shell_exec($portCommand);
+
+    // Vérification du résultat de la commande de port
+    if (strpos($portOutput, 'succeeded') !== false) {
+        echo '<div style="color: red; font-weight: bold;">Échec de l\'arrêt du service.</div>';
     } else {
-        echo '<div style="color: red; font-weight: bold;">Échec de la fermeture de la partie.</div>';
+        echo '<div style="color: green; font-weight: bold;">Le service a été arrêté.</div>';
     }
 } else {
     echo '<div style="color: red; font-weight: bold;">Requête invalide.</div>';
