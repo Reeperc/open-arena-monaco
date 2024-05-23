be868a3dbb464b214bc31cbfc76636ec530b1454
AntoinePh69 <antoine.philippe@groupe-esigelec.org>
2024-05-23 13:00:50 +0200

    team

--- add_bot.php
+++ add_bot.php
@@ -2,9 +2,10 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $botName = $_POST['bot_name'];
     $botLevel = $_POST['bot_level'];
+    $botTeam = $_POST['bot_team'];
 
     // Construction de la commande sshpass pour ajouter le bot dans le screen
-    $command = "sshpass -p 'quake' ssh -o StrictHostKeyChecking=no quake@195.221.30.65 'screen -S openarena-server -X stuff \"addbot \\\"$botName\\\" \\\"$botLevel\\\"^M\"'";
+    $command = "sshpass -p 'quake' ssh -o StrictHostKeyChecking=no quake@195.221.30.65 'screen -S openarena-server -X stuff \"addbot \\\"$botName\\\" \\\"$botLevel\\\" \\\"$botTeam\\\"^M\"'";
 
     // Exécution de la commande avec shell_exec
     $output = shell_exec($command);
