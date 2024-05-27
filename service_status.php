ec4b266b2d44644cb420ab67f39b7e7080a1b188
AntoinePh69 <antoine.philippe@groupe-esigelec.org>
2024-05-27 12:16:57 +0200

    ty

--- service_status.php
+++ service_status.php
@@ -16,61 +16,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     }
 
-    // Fonction pour vérifier si le serveur répond au ping
-    function isServerUp($serverIP) {
-        $pingresult = shell_exec("ping -c 1 " . escapeshellarg($serverIP));
-        error_log("Ping result: $pingresult");
-        if (strpos($pingresult, '1 received') !== false) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    // Fonction pour vérifier si le site web répond au ping
-    function isWebsiteUp($website) {
-        $hostname = parse_url($website, PHP_URL_HOST);
-        if ($hostname === null) {
-            error_log("Invalid URL: $website");
-            return false;
-        }
-        $pingresult = shell_exec("ping -c 1 " . escapeshellarg($hostname));
-        error_log("Website ping result: $pingresult");
-        if (strpos($pingresult, '1 received') !== false) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    // Vérifications
-    $serverUp = isServerUp($serverIP);
+    // Vérification
     $portOpen = isPortOpen($serverIP, $port);
-    $websiteUp = isWebsiteUp($website);
 
     // Générer le message de statut
     $statusMessage = "<div style='text-align: center; margin: 20px; font-family: Arial, sans-serif;'>";
     $statusMessage .= "<h3>Statut du serveur:</h3>";
 
-    if ($serverUp) {
-        $statusMessage .= "<p style='color: green;'>Le serveur de jeu est UP.</p>";
-        if ($portOpen) {
-            $statusMessage .= "<p style='color: green;'>Le port $port est ouvert. Une partie est en cours.</p>";
-        } else if ($port != 27961) {
-            // Seulement afficher ce message si le port n'est pas 27961
-            $statusMessage .= "<p style='color: red;'>Le port $port est fermé. Aucune partie en cours.</p>";
-        }
-
-        if ($websiteUp) {
-            // Seulement afficher ce message si l'URL n'est pas celle à exclure
-            if ($website !== "http://195.221.30.16") {
-                $statusMessage .= "<p style='color: green;'>Le site web est UP. <a href=\"$website\" target=\"_blank\">Visitez le site web</a>.</p>";
-            }
-        } else {
-            $statusMessage .= "<p style='color: red;'>Le site web est DOWN.</p>";
-        }
+    if ($portOpen) {
+        $statusMessage .= "<p style='color: green;'>Le port $port est ouvert. Une partie est en cours.</p>";
     } else {
-        $statusMessage .= "<p style='color: red;'>Le serveur de jeu est DOWN.</p>";
+        $statusMessage .= "<p style='color: red;'>Le port $port est fermé. Aucune partie en cours.</p>";
     }
 
     $statusMessage .= "</div>";
