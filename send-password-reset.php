33cfbfadf73caee7602184f30419411768ec98ac
Xortann <amart@PC-de-Aurlien.(none)>
2024-05-27 14:36:03 +0200

    dd

--- send-password-reset.php
+++ send-password-reset.php
@@ -20,10 +20,10 @@ if (!endsWith($email, '@arena-monaco.fr')) {
 $token = bin2hex(openssl_random_pseudo_bytes(16));
 
 // Configuration pour l'accès à l'Active Directory
-$ldap_server = "ldaps://dc.arena-monaco.fr"; // Assurez-vous que ldaps:// est correctement configuré
-$ldap_user = 'cn=Administrateur,cn=Users,dc=arena-monaco,dc=fr'; // Nom complet de l'utilisateur administrateur LDAP
-$ldap_password = 'VotreMotDePasseLDAP'; // Mot de passe de l'utilisateur administrateur LDAP
-$ldap_base_dn = 'dc=arena-monaco,dc=fr'; // Base DN de votre Active Directory
+$ldap_server = "ldaps://dc.arena-monaco.fr";
+$ldap_user = 'cn=Administrateur,cn=Users,dc=arena-monaco,dc=fr';
+$ldap_password = 'VotreMotDePasseLDAP';
+$ldap_base_dn = 'dc=arena-monaco,dc=fr';
 
 // Connexion à l'Active Directory
 $ldap_conn = ldap_connect($ldap_server) or die("Impossible de se connecter au serveur LDAP.");
@@ -32,7 +32,7 @@ ldap_set_option($ldap_conn, LDAP_OPT_REFERRALS, 0);
 
 if ($ldap_conn) {
     // Authentification LDAP
-    $ldap_bind = ldap_bind($ldap_conn, $ldap_user, $ldap_password);
+    $ldap_bind = @ldap_bind($ldap_conn, $ldap_user, $ldap_password);
 
     if ($ldap_bind) {
         // Recherche de l'utilisateur par email
@@ -89,12 +89,12 @@ if ($ldap_conn) {
             echo "Utilisateur non trouvé dans Active Directory.";
         }
     } else {
-        echo "Échec de l'authentification LDAP.";
+        echo "Échec de l'authentification LDAP : " . ldap_error($ldap_conn);
     }
 
     // Fermeture de la connexion LDAP
     ldap_close($ldap_conn);
-} else {
+} else {    
     echo "Échec de la connexion au serveur LDAP.";
 }
 
