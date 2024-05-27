81152928521a9341f19ec275f67d723c8c93a589
baptisteproper <baptiste.bregeon@outlook.fr>
2024-05-27 10:51:46 +0200

    répertoire

--- enregistrer_config.php
+++ enregistrer_config.php
@@ -1,12 +1,13 @@
 <?php
         // Connexion SSH
+        //$command='oxidized';
         $connexion = ssh2_connect('195.221.30.48', 22);
         if ($connexion) {
             // Authentification SSH
             if (ssh2_auth_password($connexion, 'oxidized', 'azertyuiop')) {
-                $resultat = ssh2_exec($connexion, '/usr/local/bin/oxidized');
+                $resultat = ssh2_exec($connexion, "/usr/local/bin/oxidized");
                 if ($resultat) {
-                    echo "Fichier envoyé avec succès !";
+                    echo "configuration enregistrer";
                 } else {
                     echo "Une erreur s'est produite lors de l'envoi du fichier.";
                 }
