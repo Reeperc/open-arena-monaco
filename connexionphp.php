cf0963779e61a74b948bd2cd84d47bcf8e506d6e
baptisteproper <baptiste.bregeon@outlook.fr>
2024-05-27 11:03:38 +0200

    répertoire

--- connexionphp.php
+++ connexionphp.php
@@ -36,7 +36,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
             // Requête de recherche LDAP avec le filtre d'adresse e-mail
             $search_filter = "(mail=$email)";
-            $attributes = array("cn"); // Attribut à récupérer (CN)
+            $attributes = array("cn", "directoryName"); // Attribut à récupérer (CN)
             $search_result = ldap_search($ldap_conn, $ldap_base_dn, $search_filter, $attributes);
 
             if ($search_result_admin !== false && $search_result_organisateur !== false && $search_result != false) {
@@ -67,9 +67,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                     // Récupérer le CN de l'utilisateur trouvé
                     $cn = $entries[0]['cn'][0];
+                    $directoryName= $entries[0]['$directoryName'][0];
 
                     // Authentification réussie, enregistrer le nom d'utilisateur dans une variable de session
                     $_SESSION['joueur_username'] = $cn;
+                    $_SESSION['joueur_directory'] = $directoryName;
                     $_SESSION['Welcome_message2'] = "Bienvenue ! Connexion réussie";
 
                     // Rediriger vers la page d'accueil après la connexion réussie
