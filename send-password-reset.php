526f67817b521f763b6391a5cbe3219556804305
Xortann <amart@PC-de-Aurlien.(none)>
2024-05-27 15:34:59 +0200

    xx

--- send-password-reset.php
+++ send-password-reset.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Assurez-vous que PHPMailer est installé et chargé
 require 'vendor/autoload.php';
 
