ad340abff66ad5540a37b9d2c7a38e8b734d776b
AntoinePh69 <antoine.philippe@groupe-esigelec.org>
2024-05-27 13:00:28 +0200

    test

--- StatutDesServeurs.php
+++ StatutDesServeurs.php
@@ -35,28 +35,28 @@
     <main>
         <section class="server-grid">
             <div class="server-info" id="server-rouen">
-                <img src="images/rouen.jpg" alt="Icone Serveur Rouen" class="server-icon">
+                <img src="images/villes/rouen.jpg" alt="Icone Serveur Rouen" class="server-icon">
                 <div class="server-name">Serveur Rouen</div>
                 <div class="server-details">IP: 192.221.50.27</div>
                 <div class="server-details">URL: <a href="http://192.221.50.17" target="_blank">http://192.221.50.17</a></div>
                 <div id="status-rouen" class="server-details">Vérification en cours...</div>
             </div>
             <div class="server-info" id="server-monaco">
-                <img src="images/monaco.jpg" alt="Icone Serveur Monaco" class="server-icon">
+                <img src="images/villes/monaco.jpg" alt="Icone Serveur Monaco" class="server-icon">
                 <div class="server-name">Serveur Monaco</div>
                 <div class="server-details">IP: 195.221.30.65</div>
                 <div class="server-details">URL: <a href="http://195.221.30.16" target="_blank">http://195.221.30.16</a></div>
                 <div id="status-monaco" class="server-details">Vérification en cours...</div>
             </div>
             <div class="server-info" id="server-paris">
-                <img src="images/paris.jpg" alt="Icone Serveur Paris" class="server-icon">
+                <img src="images/villes/paris.jpg" alt="Icone Serveur Paris" class="server-icon">
                 <div class="server-name">Serveur Paris</div>
                 <div class="server-details">IP: 192.168.40.129</div>
                 <div class="server-details">URL: <a href="http://192.221.40.131" target="_blank">http://192.221.40.131</a></div>
                 <div id="status-paris" class="server-details">Vérification en cours...</div>
             </div>
             <div class="server-info" id="server-mq">
-                <img src="images/montcuq.jpg" alt="Icone Serveur MQ" class="server-icon">
+                <img src="images/villes/montcuq.jpg" alt="Icone Serveur MQ" class="server-icon">
                 <div class="server-name">Serveur MQ</div>
                 <div class="server-details">IP: 192.168.20.27</div>
                 <div class="server-details">URL: <a href="http://192.221.20.46" target="_blank">http://192.221.20.46</a></div>
