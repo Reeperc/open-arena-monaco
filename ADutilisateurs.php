211528ef3f68f6daa9dee55221b3849a2638efd3
AntoinePh69 <antoine.philippe@groupe-esigelec.org>
2024-05-28 11:53:58 +0200

    test

--- ADutilisateurs.php
+++ ADutilisateurs.php
@@ -1,46 +1,34 @@
 <?php
-// Configuration pour l'accès à l'Active Directory
+header('Content-Type: application/json');
+
 $ldap_server = 'ldap://195.221.30.4'; // Remplacez par votre serveur LDAP
 $ldap_user = 'cn=Administrateur, cn=Users, dc=arena-monaco, dc=fr'; // Remplacez par votre nom d'utilisateur LDAP
 $ldap_password = '1234567890A@'; // Remplacez par votre mot de passe LDAP
 $ldap_base_dn = 'DC=arena-monaco, DC=fr'; // Remplacez par votre base DN
 
-// Connexion à l'Active Directory
-$ldap_conn = ldap_connect($ldap_server) or die("Impossible de se connecter au serveur LDAP.");
+$ldap_conn = ldap_connect($ldap_server) or die(json_encode(['error' => 'Impossible de se connecter au serveur LDAP.']));
 ldap_set_option($ldap_conn, LDAP_OPT_PROTOCOL_VERSION, 3);
 ldap_set_option($ldap_conn, LDAP_OPT_REFERRALS, 0);
 
 if ($ldap_conn) {
-    // Authentification
     $ldap_bind = ldap_bind($ldap_conn, $ldap_user, $ldap_password);
 
     if ($ldap_bind) {
-        // Requête pour récupérer tous les utilisateurs avec un sAMAccountName
         $filter = "(objectClass=user)";
-        $attributes = array("sAMAccountName");
+        $attributes = array("samaccountname");
         $search = ldap_search($ldap_conn, $ldap_base_dn, $filter, $attributes);
         $entries = ldap_get_entries($ldap_conn, $search);
 
-        // Récupérer les noms d'utilisateurs
-        $users = [];
+        $users = array();
         for ($i = 0; $i < $entries['count']; $i++) {
             $users[] = $entries[$i]['samaccountname'][0];
         }
-
-        // Fermeture de la connexion LDAP
-        ldap_close($ldap_conn);
-
-        // Renvoyer les noms d'utilisateurs sous forme de JSON
-        header('Content-Type: application/json');
         echo json_encode($users);
-        exit;
     } else {
-        echo "Échec de l'authentification LDAP.";
+        echo json_encode(['error' => 'Échec de l\'authentification LDAP.']);
     }
-
-    // Fermeture de la connexion LDAP
     ldap_close($ldap_conn);
 } else {
-    echo "Échec de la connexion au serveur LDAP.";
+    echo json_encode(['error' => 'Échec de la connexion au serveur LDAP.']);
 }
 ?>
