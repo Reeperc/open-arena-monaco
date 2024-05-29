87d6078f43ae6db0924ecaa2c118dfb72048e509
AntoinePh69 <148543084+AntoinePh69@users.noreply.github.com>
2024-05-29 18:08:51 +0200

    Update lance_partie.php

--- lance_partie.php
+++ lance_partie.php
@@ -1,7 +1,8 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Exécutez la commande pour arrêter le warmup et démarrer la partie
-    $command = "sshpass -p 'quake' ssh -o StrictHostKeyChecking=no quake@195.221.30.65 'screen -S openarena-server -X stuff \"g_doWarmup \\\"0\\\"; wait; map_restart^M\"'";
+ 
+    $command = "sshpass -p 'quake' ssh -o StrictHostKeyChecking=no quake@195.221.30.65 'screen -dmS openarena-server openarena-server +g_warmup)=0";
+    
     $output = shell_exec($command);
 
     // Vous pouvez rediriger l'utilisateur vers une page de confirmation ou afficher un message
