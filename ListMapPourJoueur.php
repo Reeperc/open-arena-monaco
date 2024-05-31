eea216b360200a4bf51f2084365aad35e4878b73
Reeperc <taher974.karimdjee@gmail.com>
2024-05-31 03:39:25 +0200

    dfg

---
+++ ListMapPourJoueur.php
@@ -0,0 +1,198 @@
+<?php
+session_start();
+// Vérifier si l'utilisateur est connecté en tant que admin
+if (!isset($_SESSION['organisateur_username'])) {
+    // Rediriger vers la page de connexion si l'utilisateur n'est pas connecté
+    header("Location: ConnexionF.php");
+    exit();
+}
+
+if (isset($_SESSION['welcome_message'])) {
+    echo "<p style='color: green;'>" . $_SESSION['welcome_message'] . "</p>";
+    unset($_SESSION['welcome_message']); // Supprimer la variable de session après l'affichage
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liste des Cartes</title>
+</head>
+
+<body class="body2">
+    <link rel="stylesheet" href="style.css">
+    <?php include('MenuJoueurF.php'); ?>
+    <video autoplay loop muted playsinline id="background-video">
+        <source src="videos/video2.mp4" type="video/mp4">
+    </video>
+
+    <main class="main2">
+        <h1>Liste des Cartes</h1>
+        <div class="grid-container">
+            <!-- Match à mort -->
+            <section class="game-type">
+                <h2>Match à mort (Deathmatch)</h2>
+                <ul>
+                    <li>czest1dm</li>
+                    <li>chaos2</li>
+                    <li>mlca1</li>
+                    <li>oa_dm1</li>
+                </ul>
+            </section>
+
+            <!-- Capture du drapeau -->
+            <section class="game-type">
+                <h2>Capture du drapeau (CTF)</h2>
+                <ul>
+                    <li>am_lavactf</li>
+                    <li>am_lavactfxl</li>
+                    <li>am_underworks2</li>
+                    <li>cbctf1</li>
+                    <li>ctf_compromise</li>
+                    <li>ctf_gate1</li>
+                    <li>ctf_inyard</li>
+                    <li>delta</li>
+                </ul>
+            </section>
+
+            <!-- Domination -->
+            <section class="game-type">
+                <h2>Domination</h2>
+                <ul>
+                    <li>aggressor</li>
+                    <li>am_lavaarena</li>
+                    <li>ctf_gate1</li>
+                    <li>ctf_inyard</li>
+                    <li>mlca1</li>
+                </ul>
+            </section>
+
+            <!-- One-Flag CTF -->
+            <section class="game-type">
+                <h2>One-Flag CTF</h2>
+                <ul>
+                    <li>am_lavactf</li>
+                    <li>am_lavactfxl</li>
+                    <li>ctf_inyard</li>
+                    <li>delta</li>
+                    <li>delta2</li>
+                    <li>hydronext2</li>
+                </ul>
+            </section>
+
+            <!-- Harvester -->
+            <section class="game-type">
+                <h2>Harvester</h2>
+                <ul>
+                    <li>am_lavactf</li>
+                    <li>am_lavactf2</li>
+                    <li>delta</li>
+                    <li>hydronext2</li>
+                    <li>oa_bases3</li>
+                    <li>oa_Thor</li>
+                </ul>
+            </section>
+
+            <!-- Rocket Arena -->
+            <section class="game-type">
+                <h2>Rocket Arena</h2>
+                <ul>
+                    <li>aggressor</li>
+                    <li>am_lavaarena</li>
+                    <li>mlca1</li>
+                </ul>
+            </section>
+
+            <!-- Instagib -->
+            <section class="game-type">
+                <h2>Instagib</h2>
+                <ul>
+                    <li>aggressor</li>
+                    <li>oa_dm1</li>
+                </ul>
+            </section>
+        </div>
+    </main>
+
+    <!-- 
+    <h1>Liste des Cartes</h1>
+
+    <h2>Match à mort (Deathmatch)</h2>
+
+    <h2>Match à mort en équipe (Team Deathmatch)</h2>
+
+    <ul>
+        <li>czest1dm </li>
+
+        <li>chaos2 </li>
+        <li>mlca1 </li>
+        <li>oa_dm1 </li>
+    </ul>
+
+    <h2>Capture du drapeau (CTF)</h2>
+
+    <ul>
+        <li>am_lavactf</li>
+        <li>am_lavactfxl</li>
+        <li>am_underworks2</li>
+        <li>cbctf1</li>
+        <li>ctf_compromise</li>
+        <li>ctf_gate1</li>
+        <li>ctf_inyard</li>
+        <li>delta
+            <form action="Chargerdelta.php" method="get">
+                <input type="submit" value="charger">
+            </form>
+        </li>
+        <li>delta <button type="button"> Charger </button>
+        </li>
+        <li>hydronext2</li>
+        <li>oa_bases3</li>
+        <li>oa_bases7</li>
+        <li>oa_ctf2</li>
+    </ul>
+
+    <h2>Domination</h2>
+
+    <ul>
+        <li>aggressor</li>
+        <li>am_lavaarena</li>
+        <li>ctf_gate1</li>
+        <li>ctf_inyard</li>
+        <li>mlca1</li>
+    </ul>
+
+    <h2>One-Flag CTF</h2>
+
+    <ul>
+        <li>am_lavactf</li>
+        <li>am_lavactfxl</li>
+        <li>ctf_inyard</li>
+        <li>delta</li>
+        <li>hydronext2</li>
+    </ul>
+
+    <h2>Harvester</h2>
+
+    <ul>
+        <li>am_lavactf</li>
+        <li>am_lavactf2</li>
+        <li>delta</li>
+        <li>hydronext2</li>
+        <li>oa_bases3</li>
+        <li>oa_Thor</li>
+    </ul>
+    <h2>Rocket Arena</h2>
+
+    <h2>Instagib</h2> -->
+
+    <!-- <button class="form-button-retour" id="return-button" onclick="window.location.href='AccueilVisiteurF.php'">Retour</button> -->
+    <!-- <?php include('FooterF.php'); ?> -->
+</body>
+
+</html>
---
+++ ModesJeuxPourJoueur.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+
+include('database.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modes de jeu</title>
+    <!-- <link rel="stylesheet" href="style.css"> -->
+</head>
+
+<body>
+    <link rel="stylesheet" href="style.css">
+    <?php include('MenuJoueurF.php'); ?>
+    <video autoplay loop muted playsinline id="background-video">
+        <source src="videos/video3.mp4" type="video/mp4">
+    </video>
+
+    <h4>Modes de jeu</h4>
+    <div class="cards-container">
+        <div class="card">
+            <h2>Match à mort (Deathmatch)</h2>
+            <p>Les joueurs s'affrontent pour obtenir le plus de frags possible. Le premier joueur à atteindre un nombre de frags prédéterminé remporte la partie.</p>
+        </div>
+        <div class="card">
+            <h2>Match à mort en équipe (Team Deathmatch)</h2>
+            <p>Deux équipes s'affrontent pour obtenir le plus de frags possible. La première équipe à atteindre un nombre de frags prédéterminé remporte la partie.</p>
+        </div>
+        <div class="card">
+            <h2>Capture du drapeau (CTF)</h2>
+            <p>Deux équipes s'affrontent pour capturer le drapeau de l'autre équipe et le ramener à leur base. La première équipe à capturer le drapeau un certain nombre de fois remporte la partie.</p>
+        </div>
+        <div class="card">
+            <h2>Domination</h2>
+            <p>Les joueurs s'affrontent pour contrôler des points stratégiques sur la carte. L'équipe qui contrôle le plus de points à la fin du temps imparti remporte la partie.</p>
+        </div>
+        <div class="card">
+            <h2>One-Flag CTF</h2>
+            <p>Variante de Capture the Flag où il n'y a qu'un seul drapeau à capturer. La première équipe à capturer le drapeau et le ramener à sa base remporte la partie.</p>
+        </div>
+        <div class="card">
+            <h2>Harvester</h2>
+            <p>Les joueurs s'affrontent pour collecter des âmes en tuant des ennemis et en ramassant des âmes qui apparaissent sur la carte. La première équipe à atteindre un nombre d'âmes prédéterminé remporte la partie.</p>
+        </div>
+        <div class="card">
+            <h2>Rocket Arena</h2>
+            <p>Les joueurs s'affrontent uniquement avec des lance-roquettes. Le premier joueur à atteindre un nombre de frags prédéterminé remporte la partie.</p>
+        </div>
+        <div class="card">
+            <h2>Instagib</h2>
+            <p>Un mode de jeu où toutes les armes tuent en un seul coup. Le premier joueur à atteindre un nombre de frags prédéterminé remporte la partie.</p>
+        </div>
+    </div>
+
+    <!-- <button id="return-button" onclick="window.location.href='AccueilVisiteurF.php'">Retour</button> -->
+    <!-- <?php include('FooterF.php'); ?> -->
+
+</body>
+
+</html>
